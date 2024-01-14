3524536dc6faba5e2650cb51edf69595f726ebea
NikNik2023 <novitskiy_nn@mail.ru>
2024-01-14 17:55:54 +0300

    Новая версия блокнота с сохранением не локально, а в базу данных

--- link.rb
+++ link.rb
@@ -16,6 +16,25 @@ class Link < Post
   def to_strings
     time_string = "Создано: #{@created_at.strftime("%Y.%m.%d, %H:%M:%S")} \n\r \n\r"
 
-    return [@url, @text, time_string]
+    [@url, @text, time_string]
   end
+
+  def to_db_hash
+    super.merge(
+        "text" => @text,
+        "url" => @url
+    )
+  end
+
+  # Метод load_data у Ссылки считывает дополнительно url ссылки
+  def load_data(data_hash)
+    # Сперва дергаем родительский метод load_data для общих полей. Обратите
+    # внимание, что вызов без параметров тут эквивалентен super(data_hash), так
+    # как те же параметры будут переданы методы super автоматически.
+    super
+
+    # Теперь достаем из хэша специфичное только для ссылки значение url
+    @url = data_hash['url']
+  end
+
 end
